94bba694f4a36b5746e6cf1b83d72f30da2848e8
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-04-07 10:30:00 +0200

    chore: completes Task 1.1

---
+++ Cobol/Task 1/Hello.cob
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. HELLO. 
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION. 
+       PROCEDURE DIVISION. 
+      *Nedenfor kommer en display - Cobols måde at skrive i konsollen 
+       DISPLAY "HELLO, WORLD!"
+       STOP RUN.
