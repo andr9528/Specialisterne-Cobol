ae1d9211718d7406923d7696a8a840395745b027
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-04-07 10:36:07 +0200

    chore: completes Task 1.2

--- Cobol/Task 1/Hello.cob
+++ Cobol/Task 1/Hello.cob
@@ -1,8 +1,13 @@
        IDENTIFICATION DIVISION. 
-       PROGRAM-ID. HELLO. 
-       DATA DIVISION. 
+       PROGRAM-ID. HELLO.
+       
+       DATA DIVISION.
        WORKING-STORAGE SECTION. 
+       01 VAR-TEXT         
+       PIC X(30) VALUE "HELLO fra Variabel".
+
        PROCEDURE DIVISION. 
       *Nedenfor kommer en display - Cobols måde at skrive i konsollen 
-       DISPLAY "HELLO, WORLD!"
+       DISPLAY "HELLO, WORLD!".
+       DISPLAY VAR-TEXT. 
        STOP RUN.
